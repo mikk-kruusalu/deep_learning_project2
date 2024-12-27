c3660b2ffaf845705fae851574a4d6088f247504
Mikk Kruusalu <kruusalu.mikk@gmail.com>
2024-12-27 10:15:06 +0200

    Van der Pol equation is solved, also an inverse problem

--- pinn/vanderpol.py
+++ pinn/vanderpol.py
@@ -58,23 +58,72 @@ def solve_diffrax(t, x0, x0d, mu):
         0.01,
         (x0, x0d),
         args=mu,
-        saveat=SaveAt(ts=t),
+        saveat=SaveAt(dense=True),
     )
     return sol
 
 
+@eqx.filter_jit
+def fit_loss(pytree: tuple[FCN, Array], t: Float[Array, ""], data: Float[Array, "2 "]):
+    model, mu = pytree
+
+    x = jax.vmap(model)(t)
+    modeld = jax.grad(model)
+    modeldd = jax.grad(modeld)
+
+    lsq_loss = jnp.mean((jax.vmap(model)(data[0]) - data[1]) ** 2)
+
+    xd = jax.vmap(modeld)(t)
+    xdd = jax.vmap(modeldd)(t)
+    equation_loss = jnp.mean((xdd - mu * (1 - x**2) * xd + x) ** 2)
+    return lsq_loss + equation_loss
+
+
+@eqx.filter_jit
+def make_step_fit(pytree, optim, opt_state, t, data):
+    loss_value, grads = eqx.filter_value_and_grad(fit_loss)(pytree, t, data)
+    updates, opt_state = optim.update(
+        grads, opt_state, eqx.filter(pytree, eqx.is_array)
+    )
+    pytree = eqx.apply_updates(pytree, updates)
+    return pytree, opt_state, loss_value
+
+
+def fit_pinn(nn, t, data, mu):
+    optim = optax.adam(1e-3)
+    pytree = (nn, jnp.array(mu))
+    opt_state = optim.init(eqx.filter(pytree, eqx.is_array))
+
+    for epoch in range(15001):
+        pytree, opt_state, loss_value = make_step_fit(pytree, optim, opt_state, t, data)
+        if epoch % 500 == 0:
+            print(f"{epoch}: {loss_value} \t mu: {pytree[1]}")
+
+    return pytree
+
+
 if __name__ == "__main__":
     # set initial conditions
-    t = jnp.linspace(0, 15, 1000)
+    t = jnp.linspace(0, 8, 100)
     x0 = 2.0
     x0d = 0.0
-    mu = 0.5
+    mu = 1.2
 
+    # solve the equation
     nn = solve_pinn(FCN(32, 3, jr.key(1234)), t, x0, x0d, mu)
-
     sol = solve_diffrax(t, x0, x0d, mu)
 
+    # generate random data
+    t_rand = 5.0 * jr.uniform(jr.key(2645), shape=(20,))
+    y_rand = jax.vmap(sol.evaluate)(t_rand)[0] + 0.1 * jr.normal(
+        jr.key(6724), shape=(20,)
+    )
+    data = jnp.stack([t_rand, y_rand])
+    nn_fit, mu_fit = fit_pinn(FCN(32, 3, jr.key(5432)), t, data, 0.0)
+
     plt.plot(t, jax.vmap(nn)(t), label="PINN")
-    plt.plot(t, sol.ys[0], label="Diffrax")  # type: ignore
+    plt.plot(t, jax.vmap(sol.evaluate)(t)[0], label="Diffrax")  # type: ignore
+    plt.scatter(t_rand, y_rand, label="Noisy data", c="tab:green")
+    plt.plot(t, jax.vmap(nn_fit)(t), label="Fitted PINN")
     plt.legend()
     plt.show()
